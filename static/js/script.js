c1253baa1807c05cc90001aba6e0803d01774ab1
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-08-12 10:31:32 +0100

    Add task collapsible setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,11 @@
-// A function that will allow the Materialize mobile navbar to work
+// All JS contained in this file will run once the page has fully loaded
 $(document).ready(function () {
+
+  // A function that will allow the Materialize mobile navbar to work
   $(".sidenav").sidenav({edge: "right"});
+
+  // A function that will allow the Materialize dropdown to work
+  $('.collapsible').collapsible();
+  
 });
+
