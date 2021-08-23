9ab30fa6248ef8d249083f28ba7691d7580d1fa5
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-08-23 18:28:49 +0100

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -9,5 +9,15 @@ $(document).ready(function () {
 
   // Initalize the tooltips
   $('.tooltipped').tooltip();
+
+  // Intialize the datepicker
+  $('.datepicker').datepicker({
+    format: 'dd mmmm yyyy',
+    yearRange: 3,
+    showClearBtn: true,
+    i18n: {
+      done: 'select',
+    }
+  });
 });
 
